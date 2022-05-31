4a2f1b451838aa2f7318cf4a34777be9446b44fe
Alex Voitekhovich <a.voitekhovich@gmail.com>
2022-05-31 14:44:06 +0300

    feat: Добавить лайки для карточек

--- scripts/index.js
+++ scripts/index.js
@@ -32,28 +32,32 @@ const profileEditBtn = profile.querySelector('.profile__edit-button')
 const profileName = profile.querySelector('.profile__name');
 const profileAbout = profile.querySelector('.profile__about');
 
-const popup = document.querySelector('.popup');
-const formCloseBtn = popup.querySelector('.popup__close');
-const formSaveBtn = popup.querySelector('.popup__button');
-const editName = popup.querySelector('.popup__item_type_name');
-const editAbout = popup.querySelector('.popup__item_type_about');
+const popupEdit = document.querySelector('.popup__form_type_edit');
+const formCloseBtn = popupEdit.querySelector('.popup__close');
+const formSaveBtn = popupEdit.querySelector('.popup__button');
+const editName = popupEdit.querySelector('.popup__item_type_name');
+const editAbout = popupEdit.querySelector('.popup__item_type_about');
 
 const elementTemplate = document.querySelector('#element').content;
 const elements = page.querySelector('.elements');
 
-const form = document.querySelector('#form');
-const profileAddBtn = profile.querySelector('.profile__add-button')
+const profileAddBtn = profile.querySelector('.profile__add-button');
+const popupAdd = document.querySelector('.popup__form_type_add');
+const formAddCloseBtn = popupAdd.querySelector('.popup__close');
+const formAddSaveBtn = popupAdd.querySelector('.popup__button');
+const editAddTitle = popupAdd.querySelector('.popup__item_type_title');
+const editAddUrl = popupAdd.querySelector('.popup__item_type_url');
 
 // Попап редактирование профиля
 
 function openPopup() {
   editName.value = profileName.textContent;
   editAbout.value = profileAbout.textContent;
-  popup.classList.add('popup_visible');
+  popupEdit.classList.add('popup_visible');
 }
 
 function closePopup() {
-  popup.classList.remove('popup_visible');
+  popupEdit.classList.remove('popup_visible');
 }
 
 function savePopup(evt) {  
@@ -69,23 +73,42 @@ formSaveBtn.addEventListener('click', savePopup);
 
 // Наполнение страницы карточками
 
-for (const item of initialCards) {
+function addElement(item) {
   const element = elementTemplate.querySelector('.element').cloneNode(true);
   element.querySelector('.element__image').src = item.link;
   element.querySelector('.element__image').alt = item.name;
   element.querySelector('.element__title').textContent = item.name;
-  elements.append(element);
+  element.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+  }); 
+  return element;
+}
+
+for (const item of initialCards) {
+  elements.append(addElement(item));
 }
 
 // Попап добавления нового места
 
-function openForm() {
-  form.classList.add('popup_visible');
+function openPopupAdd() {
+  editAddTitle.value = 'Вид на горы';
+  editAddUrl.value = 'https://images.unsplash.com/photo-1653754070622-0d6041a063cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60';
+  popupAdd.classList.add('popup_visible');
 }
 
-function closeForm() {
-  form.classList.remove('popup_visible');
+function closePopupAdd() {
+  popupAdd.classList.remove('popup_visible');
 }
 
-profileAddBtn.addEventListener('click', openForm);
-formCloseBtn.addEventListener('click', closeForm);
+function savePopupAdd(evt) {  
+  evt.preventDefault();
+  const item = {}
+  item.name = editAddTitle.value;
+  item.link = editAddUrl.value;
+  elements.prepend(addElement(item));
+  closePopupAdd()
+}
+
+profileAddBtn.addEventListener('click', openPopupAdd);
+formAddCloseBtn.addEventListener('click', closePopupAdd);
+formAddSaveBtn.addEventListener('click', savePopupAdd);
