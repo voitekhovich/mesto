739c4aa5aa024700d99595a6889431f3fbe583e3
Alex Voitekhovich <a.voitekhovich@gmail.com>
2022-05-31 07:41:52 +0300

    feat: Добавление карточек через JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const page = document.querySelector('.page');
 
 const profile = page.querySelector('.profile');
@@ -10,6 +37,14 @@ const formCloseBtn = popup.querySelector('.popup__close');
 const formSaveBtn = popup.querySelector('.popup__button');
 const editName = popup.querySelector('.popup__item_type_name');
 const editAbout = popup.querySelector('.popup__item_type_about');
+
+const elementTemplate = document.querySelector('#element').content;
+const elements = page.querySelector('.elements');
+
+const form = document.querySelector('#form');
+const profileAddBtn = profile.querySelector('.profile__add-button')
+
+// Попап редактирование профиля
 
 function openPopup() {
   editName.value = profileName.textContent;
@@ -31,3 +66,26 @@ function savePopup(evt) {
 profileEditBtn.addEventListener('click', openPopup);
 formCloseBtn.addEventListener('click', closePopup);
 formSaveBtn.addEventListener('click', savePopup);
+
+// Наполнение страницы карточками
+
+for (const item of initialCards) {
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__image').src = item.link;
+  element.querySelector('.element__image').alt = item.name;
+  element.querySelector('.element__title').textContent = item.name;
+  elements.append(element);
+}
+
+// Попап добавления нового места
+
+function openForm() {
+  form.classList.add('popup_visible');
+}
+
+function closeForm() {
+  form.classList.remove('popup_visible');
+}
+
+profileAddBtn.addEventListener('click', openForm);
+formCloseBtn.addEventListener('click', closeForm);
